93f99e661ad0625f38cde9a0ffe22bdb4d1448b0
Alexandr Rubnubnoff <rubnubnoff@yandex.ru>
2019-06-18 01:10:48 +0400

    starDB-app

---
+++ src/components/item-details/item-details.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
+
+import './item-details.css';
+
+const Record = ({ item, field, label }) => {
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{ item[field] }</span>
+    </li>
+  );
+};
+
+export {
+  Record
+};
+
+export default class ItemDetails extends Component {
+
+  state = {
+    item: null,
+    image: null,
+    loading: false,
+    error: false
+  };
+
+  componentDidMount() {
+    this.updateItem();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.itemId !== prevProps.itemId ||
+      this.props.getData !== prevProps.getData ||
+      this.props.getImageUrl !== prevProps.getImageUrl) {
+      this.updateItem();
+    }
+  }
+
+  updateItem() {
+    const { itemId, getData, getImageUrl } = this.props;
+    if (!itemId) {
+      return;
+    }
+    this.setState({ loading: true });
+    getData(itemId)
+      .then((item) => {
+        this.setState({
+          item,
+          image: getImageUrl(item),
+          loading: false,
+        });
+      })
+      .catch((e) => this.setState({ error: true }));
+  }
+
+  render() {
+
+    const { item, image, loading, error } = this.state;
+    if (loading) {
+      return <Spinner />
+    }
+    if (error) {
+      return <ErrorIndicator />
+    }
+    if (!item) {
+      return <span>Select a item from a list</span>;
+    }
+    const { name } = item;
+
+    return (
+      <div className="item-details card">
+        <img className="item-image"
+          src={image}
+          alt="item"/>
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            {
+              React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, { item });
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
